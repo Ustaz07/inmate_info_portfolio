4f43f57af6ffd4ca60cba111249ca7e3930b5a9e
Ustaz07 <shamsutukor@gmail.com>
2024-11-12 02:28:43 +0000

    index header page

---
+++ includes/index_header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" media="all" type="text/css" href="../css/bootstrap-theme.css">
+<link rel="stylesheet" media="all" type="text/css" href="../css/bootstrap-theme.css.map">
+<link rel="stylesheet" media="all" type="text/css" href="../css/bootstrap-theme.min.css">
+<link rel="stylesheet" media="all" type="text/css" href="../css/bootstrap-theme.min.css.map">
+<link rel="stylesheet" media="all" type="text/css" href="../css/bootstrap.css">
+<link rel="stylesheet" media="all" type="text/css" href="../css/bootstrap.css.map">
+<link rel="stylesheet" media="all" type="text/css" href="../css/bootstrap.min.css">
+<link rel="stylesheet" media="all" type="text/css" href="../css/bootstrap.min.css.map">
+<script src="../js/bootstrap.js"></script>
+<!--<script src="../js/bootstrap.min.js"></script>
+<script src="../js/npm.js"></script>-->
+<script src="jquery/jquery-3.0.0.js"></script>
+<!--UnComment here when site is ready-->
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+
+</head>
+
+
+ <body>
+	<div class="container">
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+     <img src="images/noor.jpg" class="img-circle" alt="logo" width="50" height="50" style="margin: 5px;">
+      </div>
+      <h2 class="text-primary text-center" style="color: #FFFFFF; font-weight: bolder; font-family: Verdana, sans-serif;">WELCOME</h2>
+   </div>
+</nav>
+	 </div>
+	</body>
+	
+</html>
