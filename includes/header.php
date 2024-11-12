9a81de48b45d57fd4900f91cdc21b210f04d960a
Ustaz07 <shamsutukor@gmail.com>
2024-11-12 02:27:02 +0000

    header page

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" media="all" type="text/css" href="css/bootstrap-theme.css">
+<link rel="stylesheet" media="all" type="text/css" href="css/bootstrap-theme.css.map">
+<link rel="stylesheet" media="all" type="text/css" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" media="all" type="text/css" href="css/bootstrap-theme.min.css.map">
+<link rel="stylesheet" media="all" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" media="all" type="text/css" href="css/bootstrap.css.map">
+<link rel="stylesheet" media="all" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" media="all" type="text/css" href="css/bootstrap.min.css.map">
+<script src="js/bootstrap.js"></script>
+</head>
+
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  		<div class="container-fluid">
+    		<span id="head-title"><img style="float: left; border-radius:50%" src="images/noor.jpg" width="35" height="35"></span>
+    		<h3 style="color: white; margin-left: 5px;"><b>Estate Agents</b></h3>
+   			
+        </div>
+	</nav>
+</body>
+</html>
