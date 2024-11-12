ee1b7c8e378dadeb444728d155fffa7848eacb78
Ustaz07 <shamsutukor@gmail.com>
2024-11-12 02:20:56 +0000

    constant page

---
+++ inc/constant.php
@@ -0,0 +1,8 @@
+<?php 
+
+              define("HOST","localhost");//host name
+              define("USER","root");//user name
+              define("PASS",'');//pasword
+              define("DB","miss");//database name
+
+?>
